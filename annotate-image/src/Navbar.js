a31fab4f7e1349428bf8f1e8a8105d6d021b7111
martaadamczyk0101 <56135679+martaadamczyk0101@users.noreply.github.com>
2022-10-19 19:26:34 +0200

    img-state-array

--- annotate-image/src/Navbar.js
+++ annotate-image/src/Navbar.js
@@ -4,22 +4,38 @@ function Navbar() {
 
   const [selectedFiles, setSelectedFiles]= useState(null)
   const [imageFiles, setImageFiles]=useState([])
-  const [loaded, setLoaded]= useState(0)
+  const [currentImgID, setCurrentImgID]=useState(2)
+  const [currentImgName, setCurrentImgName]=useState('IMG_2640.JPG')
+  const [imgNamesList, setImgNamesList]=useState([])
   var id=0;
+  var imgNames=[]
+  var imgNamesTest = [{id:0, name:'IMG_2640.JPG'}, {id:1, name:'dddd.jpg'}, {id:2, name:'aaa.png'}]
 
   useEffect(() => {
+    if(selectedFiles){
     handleSubmit(selectedFiles); 
-    console.log(selectedFiles)
+    }
   }, [selectedFiles]);
 
   useEffect(() => {
-    console.log(imageFiles)
-  }, [imageFiles]);
+    console.log(imgNamesList)
+  }, [imgNamesList]);
+
+  const changeCurrentImg = () =>{
+    for(var x = 0; x<imgNames.length; x++) {
+      if(currentImgID===imgNames[x].id){
+        setCurrentImgName(imgNames[x].name)
+      }
+  }
+  }
 
   const handleSubmit = async (event) => {
     const data = new FormData()
     for(var x = 0; x<selectedFiles.length; x++) {
         data.append('file', selectedFiles[x])
+        imgNames.push({id:id, name:selectedFiles[x].name})
+        id=id+1
+        getImgName(selectedFiles[x])
     }
 
     const response = await fetch('http://localhost:4000/image', {
@@ -30,10 +46,13 @@ function Navbar() {
     if (response) console.log(response.statusText)
   }
 
+  const getImgName = (imgData) => {
+    setImgNamesList(imgNamesList=> [...imgNamesList, {id: id, name: imgData.name}])
+    id= id+1
+  }
+
   const handleFileChange = (event) => {
     setSelectedFiles(event.target.files)
-    setImageFiles([...imageFiles, {id: id, img: event.target.files[0].name}])
-    id= id+1
   }
 
   return (
@@ -46,6 +65,7 @@ function Navbar() {
           </label>
           <input type='file' accept="image/png, image/jpg, image/jpeg" id='upload-btn' name='file' multiple onChange={handleFileChange}></input>
         </form>
+
         <label id='download-btn'><button className='download-btn'></button>Download</label>
         <label id='rectangle-btn'><button></button>Rectangle</label>
         <label id='polygon-btn'><button></button>Polygon</label>
@@ -54,6 +74,12 @@ function Navbar() {
         <label id='clear-btn'><button></button>Clear All</label>
         <label id='tbd-btn'><button></button>tbd</label>
         <label id='tbd2-btn'><button></button>tbd</label>
+
+        <button onClick= {changeCurrentImg}>elo</button>
+
+          {/* <img src={require(currentImgName)}/> */}
+          <img src={require('./images/' + imgNamesTest[0].name)}></img>
+          
 
     </div>
   )
--- backend/app.js
+++ backend/app.js
@@ -6,7 +6,7 @@ const multer = require('multer')
 
 const storage = multer.diskStorage({
     destination: (req, file, cb) => {
-        cb(null, 'images/')
+        cb(null, '../annotate-image/src/images/')
     },
     filename: (req, file, cb) => {
         cb(null, file.originalname)
