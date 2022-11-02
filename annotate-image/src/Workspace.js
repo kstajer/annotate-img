5ef9cef59d214299feb1aaf452a167f0c5d0c277
Karol Stajer <kastajer1@gmail.com>
2022-11-03 00:45:55 +0100

    canvas fit img, but no resizing

--- annotate-image/src/Workspace.js
+++ annotate-image/src/Workspace.js
@@ -1,12 +1,11 @@
 import React from 'react'
-import { useState, useEffect, useRef} from 'react';
+import { useState, useEffect, useRef } from 'react';
 import Simple from "./demo";
-import test_img from './images/dog.jpeg';
 
-function Workspace({imgNames}) {
+function Workspace({ imgNames }) {
 
-  const [currentImgID, setCurrentImgID]= useState(-1)
-  const [currentImgName, setCurrentImgName]= useState(null)
+  const [currentImgID, setCurrentImgID] = useState(-1)
+  const [currentImgName, setCurrentImgName] = useState(null)
 
   const [imgHeight, setImgHeight] = useState(0);
   const [imgWidth, setImgWidth] = useState(0);
@@ -24,39 +23,59 @@ function Workspace({imgNames}) {
     setWorkspaceWidth(workspaceRef.current.clientWidth)
   })
 
-  var imgSlide=0;
+  function resizeContainer() {
+    setWorkspaceHeight(workspaceRef.current.clientHeight);
+    setWorkspaceWidth(workspaceRef.current.clientWidth);
+
+    if ((workspaceHeight / workspaceWidth) < (imgHeight / imgWidth)) {
+      setOffsetHeight('100%');
+      var scale = workspaceHeight / imgHeight
+      if (scale < 1) {
+        setOffsetWidth((imgWidth * scale) + 'px');
+      }
+      else if (scale > 1) {
+        setOffsetWidth(0);
+      }
+
+    }
+    else if ((workspaceHeight / workspaceWidth) > (imgHeight / imgWidth)) {
+      setOffsetWidth('100%');
+      var scale = workspaceWidth / imgWidth
+      if (scale < 1) {
+        setOffsetHeight((imgHeight * scale) + 'px');
+      }
+      else if (scale > 1) {
+        setOffsetHeight(0);
+      }
+
+    }
+    else if ((workspaceHeight / workspaceWidth) == (imgHeight / imgWidth)) {
+      setOffsetWidth('100%');
+      setOffsetHeight('100%');
+    }
+    console.log('------------------')
+    console.log('imgName: ' + currentImgName)
+    console.log('scale: ' + scale)
+    console.log('imgWidth: ' + imgWidth + ', imgHeight: ' + imgHeight)
+    console.log('img_ratio (H/W) = ' + Math.round((imgHeight / imgWidth) * 10000) / 10000);
+    console.log('workspaceWidth: ' + workspaceWidth + ', workspaceHeight: ' + workspaceHeight)
+    console.log('workspace_ratio (H/W) = ' + Math.round((workspaceHeight / workspaceWidth) * 10000) / 10000);
+    console.log('offsetWidth: ' + offsetWidth + ', offsetHeight: ' + offsetHeight)
+  }
+
+  var imgSlide = 0;
 
   useEffect(() => {
     function handleResize() {
-      setWorkspaceHeight(workspaceRef.current.clientHeight);
-      setWorkspaceWidth(workspaceRef.current.clientWidth);
-
-      if ((workspaceHeight / workspaceWidth) < (imgHeight / imgWidth)) {
-        setOffsetHeight('100%');
-        var scale = workspaceHeight / imgHeight;
-        setOffsetWidth((imgWidth * scale) + 'px');
-      }
-      else if ((workspaceHeight / workspaceWidth) > (imgHeight / imgWidth)) {
-        setOffsetWidth('100%');
-        var scale = workspaceWidth / imgWidth;
-        setOffsetWidth((imgHeight * scale) + 'px');
-      }
-      else if ((workspaceHeight / workspaceWidth) == (imgHeight / imgWidth)) {
-        setOffsetWidth('100%');
-        setOffsetHeight('100%');
-      }
-
-      console.log('img_ratio (H/W) = ' + Math.round((imgHeight / imgWidth) * 100) / 100);
-      console.log('workspace_ratio (H/W) = ' + Math.round((workspaceHeight / workspaceWidth) * 100) / 100);
-  }
+      resizeContainer();
+    }
     window.addEventListener('resize', handleResize)
   })
 
   useEffect(() => {
-    console.log(imgNames)
-    if(imgSlide===1){
+    if (imgSlide === 1) {
       nextImg()
-      imgSlide+=1
+      imgSlide += 1
     }
   }, [imgNames]);
 
@@ -64,17 +83,23 @@ function Workspace({imgNames}) {
     console.log('id: ' + currentImgID + ' name: ' + currentImgName)
   }, [currentImgID]);
 
+  useEffect(() => {
+    resizeContainer()
+  }, [imgHeight, imgWidth]);
+
   const nextImg = () => {
-    if(currentImgID < imgNames.length-1){
-    setCurrentImgID(currentImgID + 1)
-    setCurrentImgName(imgNames[currentImgID+1].name)
+    if (currentImgID < imgNames.length - 1) {
+      setCurrentImgID(currentImgID + 1)
+      setCurrentImgName(imgNames[currentImgID + 1].name)
+      resizeContainer();
+    }
   }
-}
 
   const previousImg = () => {
-    if(currentImgID > 0){
-    setCurrentImgID(currentImgID - 1)
-    setCurrentImgName(imgNames[currentImgID-1].name)
+    if (currentImgID > 0) {
+      setCurrentImgID(currentImgID - 1)
+      setCurrentImgName(imgNames[currentImgID - 1].name)
+      resizeContainer();
     }
   }
 
@@ -84,16 +109,17 @@ function Workspace({imgNames}) {
     setImgHeight(naturalHeight);
   };
 
+
   return (
-    <div className='workspace' onresize= {()=> {console.log('resied')}}>
+    <div className='workspace' onresize={() => { console.log('resied') }}>
       <button className='previous-btn' onClick={previousImg}>[</button>
       <div className='image-wrapper' ref={workspaceRef}>
-        <div className='image-container' style={{ height: offsetHeight, width: offsetWidth }}>
-          { currentImgName &&
-          <>
-            <Simple img={require(`${'./images/' + currentImgName}`)}/>
-            <img onLoad={onImgLoad} src={require(`${'./images/' + currentImgName}`)} className='hide' ref={imgRef}></img>
-          </>
+        <div className='image-container' style={{ height: offsetHeight ? offsetHeight : '', width: offsetWidth ? offsetWidth : '' }}>
+          {currentImgName &&
+            <>
+              <Simple img={require(`${'./images/' + currentImgName}`)} />
+              <img onLoad={onImgLoad} src={require(`${'./images/' + currentImgName}`)} className='hide' ref={imgRef}></img>
+            </>
           }
         </div>
       </div>
